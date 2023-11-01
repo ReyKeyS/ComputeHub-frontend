62a69a448c7c96677cf52ec3826f4e88b76539d2
ReyKeyS <104489667+ReyKeyS@users.noreply.github.com>
2023-11-01 15:55:06 +0700

    minus Chakra plus MUI

--- src/main.jsx
+++ src/main.jsx
@@ -3,13 +3,8 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 
-// Plugin ChakraUI
-import { ChakraProvider } from '@chakra-ui/react'
-
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <ChakraProvider>
-      <App />
-    </ChakraProvider>
+  <React.StrictMode>    
+    <App />
   </React.StrictMode>,
 )
