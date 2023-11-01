880302ac85c67a31477dac32662ce1014d981480
PaddyHermanito <paddy.hermanito.suhartanto93@gmail.com>
2023-11-01 14:42:19 +0700

    add RegisterPage

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -19,16 +19,16 @@ function LoginPage() {
             <div class="text-white text-xl mb-1">Password</div>
             <input type="password" name="pass" placeholder="Enter your password" class="px-3 py-2 border rounded-xl w-full h-14 text-xl shadow focus:ring-2 focus:ring-blue-500 focus:outline-none"></input>
             {/* <div class="text-xl text-center mt-3">Doesn't have an account? &nbsp;<a class="font-semibold text-blue-400 duration-500 hover:text-purple-600 hover:duration-500">Register Now!</a></div> */}
-            <div class="text-white text-xl text-center mt-3">Doesn't have an account? &nbsp;<a class="font-semibold text-oranye">Register Now!</a></div>
+            <div class="text-white text-xl text-center mt-3">Doesn't have an account? &nbsp;<Link to="/register"><a class="font-semibold text-oranye">Register Now!</a></Link></div>
             <div class="flex">
               <Link to="/">
                 {/* <button type="submit" name="home" formaction="index.php" class="my-5 text-lg font-bold mr-auto px-7 py-3 flex text-white rounded-full bg-gradient-to-r from-purple-700 to-blue-600 hover:bg-gradient-to-r hover:from-purple-900 hover:to-blue-800 active:bg-sky-600 focus:ring-4 focus:ring-purple-800"> */}
-                <button type="submit" name="home" formaction="index.php" class="my-5 text-lg font-bold mr-auto px-7 py-3 flex text-abu-super-gelap rounded-lg bg-oranye">
+                <button type="submit" name="home" class="my-5 text-lg font-extrabold mr-auto px-7 py-3 flex text-abu-super-gelap rounded-lg bg-oranye">
                   HOME
                 </button>
               </Link>
               {/* <button type="submit" name="login" class="my-5 text-lg font-bold ml-auto px-7 py-3 text-white rounded-full bg-gradient-to-r from-purple-700 to-blue-600 hover:bg-gradient-to-r hover:from-purple-900 hover:to-blue-800 active:bg-sky-600 focus:ring-4 focus:ring-sky-200">LOGIN</button> */}
-              <button type="submit" name="login" class="my-5 text-lg font-bold ml-auto px-7 py-3 text-abu-super-gelap rounded-lg bg-oranye">LOGIN</button>
+              <button type="submit" name="login" class="my-5 text-lg font-extrabold ml-auto px-7 py-3 text-abu-super-gelap rounded-lg bg-oranye">LOGIN</button>
             </div>
           </div>
         </div>
