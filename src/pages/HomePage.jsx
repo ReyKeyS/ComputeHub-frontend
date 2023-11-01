3018bbbc4f6a1c68dc27d56baa7415c7c37c0943
PaddyHermanito <paddy.hermanito.suhartanto93@gmail.com>
2023-11-01 19:39:15 +0700

    add gambar HomePage

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -5,9 +5,16 @@ import Header from '../component/Header'
 function HomePage() {
   return (
     <>
-        <Header></Header>
-        {/* <NavLink digunakan ketika ingin styling Navbar (active) /> */}
-        {/* <NavLink
+      <Header />
+      <div className='w-screen h-[768px] bg-[url(https://source.unsplash.com/1920x768/?tech)]'></div>
+      {/* <div className="bg-cover"
+        style={{
+          backgroundImage: 'url(https://source.unsplash.com/1920x1080/?tech)',
+          // Atur kelas CSS Tailwind lainnya di sini sesuai kebutuhan
+        }}></div> */}
+      {/* <img src="https://source.unsplash.com/1920x768/?tech" alt="" /> */}
+      {/* <NavLink digunakan ketika ingin styling Navbar (active) /> */}
+      {/* <NavLink
             className={({ isActive, isPending }) =>
                 (isActive?'text-white':'text-black')} 
             to="/"
