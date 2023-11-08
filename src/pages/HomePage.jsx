6773d1f29fdbdc476264c6985a1714536ea89213
PaddyHermanito <105158168+PaddyHermanito@users.noreply.github.com>
2023-11-08 09:48:30 +0700

    ganti gambar + add margin + animasi HomePage.jsx

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -13,40 +13,44 @@ function HomePage() {
   return (
     <>
       <Header />
-      <div className='w-screen h-[768px] flex flex-col-reverse bg-cover bg-no-repeat bg-fixed bg-[url(https://source.unsplash.com/1920x768/?tech)]'>
-        <div className='w-1/4 h-10 mx-auto bg-white text-center rounded-t-lg font-bold text-3xl'>
-          Scroll Down
+      {/* <div className='h-[768px] flex flex-col-reverse bg-cover bg-no-repeat bg-fixed bg-[url(https://source.unsplash.com/1920x768/?tech)]'> */}
+      <div className='h-[768px] grid grid-cols-2 bg-cover bg-no-repeat bg-fixed bg-[url("/img/banner_home_non_tulis.png")]'>
+        <div className='grid grid-rows-4'>
+          <span className='row-start-2 text-white text-7xl font-bold'>One Stop Store for Computer Section</span>
         </div>
+        {/* <div className='w-1/4 h-10 mx-auto bg-white text-center rounded-t-lg font-bold text-3xl'>
+          Scroll Down
+        </div> */}
       </div>
-      <div className='font-black text-4xl mt-10'>
+      <div className='font-black text-4xl mt-10 mx-20'>
         Category <WorkspacePremiumIcon fontSize='large' />
       </div>
-      <div className="flex gap-4">
-        <div className='bg-black w-44 h-60 text-white text-xl font-bold grid place-items-center rounded-2xl bg-[url(https://source.unsplash.com/176x240/?tech)]'>
-          <a>MotherBoard</a>
+      <div className="flex gap-4 snap-x mx-20">
+        <div className='bg-black w-72 h-96 text-white text-xl font-bold grid place-items-center rounded-2xl bg-[url("/img/Motherboard_non_tulis.png")] grayscale hover:grayscale-0 hover:duration-300 group'>
+          <a className='group-hover:scale-110 group-hover:duration-300'>MotherBoard</a>
         </div>
-        <div className='bg-black w-44 h-60 text-white text-xl font-bold grid place-items-center rounded-2xl bg-[url(https://source.unsplash.com/176x240/?tech)]'>
-          <a>Processor</a>
+        <div className='bg-black w-72 h-96 text-white text-xl font-bold grid place-items-center rounded-2xl bg-[url("/img/processor.png")] grayscale hover:grayscale-0 hover:duration-300 group'>
+          <a className='group-hover:scale-110 group-hover:duration-300'>Processor</a>
         </div>
-        <div className='bg-black w-44 h-60 text-white text-xl font-bold grid place-items-center rounded-2xl bg-[url(https://source.unsplash.com/176x240/?tech)]'>
-          <a>Graphic Card</a>
+        <div className='bg-black w-72 h-96 text-white text-xl font-bold grid place-items-center rounded-2xl bg-[url("/img/graphic_card.png")] grayscale hover:grayscale-0 hover:duration-300 group'>
+          <a className='group-hover:scale-110 group-hover:duration-300'>Graphic Card</a>
         </div>
-        <div className='bg-black w-44 h-60 text-white text-xl font-bold grid place-items-center rounded-2xl bg-[url(https://source.unsplash.com/176x240/?tech)]'>
-          <a>RAM</a>
+        <div className='bg-black w-72 h-96 text-white text-xl font-bold grid place-items-center rounded-2xl bg-[url("/img/ram.png")] grayscale hover:grayscale-0 hover:duration-300 group'>
+          <a className='group-hover:scale-110 group-hover:duration-300'>RAM</a>
         </div>
-        <div className='bg-black w-44 h-60 text-white text-xl font-bold grid place-items-center rounded-2xl bg-[url(https://source.unsplash.com/176x240/?tech)]'>
-          <a>PC Case</a>
+        <div className='bg-black w-72 h-96 text-white text-xl font-bold grid place-items-center rounded-2xl bg-[url("/img/pccase.png")] grayscale hover:grayscale-0 hover:duration-300 group'>
+          <a className='group-hover:scale-110 group-hover:duration-300'>PC Case</a>
         </div>
-        <div className='bg-black w-44 h-60 text-white text-xl font-bold grid place-items-center rounded-2xl bg-[url(https://source.unsplash.com/176x240/?tech)]'>
-          <a>PSU</a>
+        <div className='bg-black w-72 h-96 text-white text-xl font-bold grid place-items-center rounded-2xl bg-[url("/img/psu.png")] grayscale hover:grayscale-0 hover:duration-300 group'>
+          <a className='group-hover:scale-110 group-hover:duration-300'>PSU</a>
         </div>
       </div>
-      <div className='font-black text-4xl mt-10'>
+      <div className='font-black text-4xl mt-10 mx-20'>
         Best Deals for you
       </div>
-      <div className='flex'>
+      <div className='flex mx-20'>
         <div className='bg-abu-gelap w-60 h-80 rounded-xl flex flex-col border-2 '>
-          <div className='h-2/3 flex flex-col-reverse bg-white bg-[url(https://source.unsplash.com/200x400/?tech)]'>
+          <div className='h-2/3 flex flex-col-reverse bg-white bg-[url("/img/graphic_card.png")]'>
             <div className='grid grid-cols-3 gap-1'>
               <div className='bg-oranye font-bold m-1 text-abu-super-gelap text-center rounded-lg'>PROMO!</div>
               <div className='col-start-3 font-black'>5<StarIcon sx={{color: yellow[500]}}/></div>
