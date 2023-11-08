2f7867fdf09776ec132d93ba29d177359ad11269
PaddyHermanito <105158168+PaddyHermanito@users.noreply.github.com>
2023-11-08 19:41:12 +0700

    add shop list

--- src/pages/Shop.jsx
+++ src/pages/Shop.jsx
@@ -3,6 +3,8 @@ import Header from "../components/Header"
 import * as React from 'react';
 import Rating from '@mui/material/Rating';
 import Stack from '@mui/material/Stack';
+import StarIcon from '@mui/icons-material/Star';
+import { yellow } from '@mui/material/colors';
 function Shop(params) {
     return (
         <>
@@ -13,12 +15,131 @@ function Shop(params) {
                 <div className="w-1/2 h-96 bg-abu-super-gelap rounded-xl grid justify-center">
                     <span className="text-putih text-3xl font-bold text-center">Filter</span>
                     <span className="text-putih text-lg font-bold">Harga (Rp)</span>
-                    <input type="text" placeholder="Harga Minimum" className="border rounded-lg w-3/4 h-1/2"/>
-                    <input type="text" placeholder="Harga Maximum" className="border rounded-lg mt-5 w-3/4 h-1/2"/>
-                    <span>Rating</span>
+                    <input type="text" placeholder="Harga Minimum" className="border rounded-lg w-3/4 h-1/2" />
+                    <input type="text" placeholder="Harga Maximum" className="border rounded-lg mt-5 w-3/4 h-1/2" />
+                    <span className="text-putih">Rating</span>
+                    <div className="flex">
+                        <input type="checkbox" />
                     <Stack spacing={1}>
-                        <Rating name="half-rating" defaultValue={2.5} precision={0.5}/>
+                        <Rating name="half-rating" defaultValue={2.5} precision={0.5} />
                     </Stack>
+
+                    </div>
+                </div>
+                <div className="col-span-2 grid grid-cols-4">
+                    {/* BUAT KOMPONEN */}
+                    <div className='bg-abu-gelap w-48 h-60 rounded-xl flex flex-col border-2 '>
+                        <div className='h-2/3 flex flex-col-reverse rounded-xl bg-white bg-[url("/img/graphic_card.png")] bg-center'>
+                            <div className='grid grid-cols-3 gap-1'>
+                                <div className='col-start-3 font-black'>5<StarIcon sx={{ color: yellow[500] }} /></div>
+                            </div>
+                        </div>
+                        <div className='h-1/3'>
+                            <div className='grid grid-rows-4'>
+                                <div className='row-span-2 text-white font-bold text-xl'>VGA ASUS ROG</div>
+                                <div className='text-oranye font-bold text-lg'>Rp 9.000.000</div>
+                            </div>
+                        </div>
+                    </div>
+                    {/* INI BERULANG */}
+                    <div className='bg-abu-gelap w-48 h-60 rounded-xl flex flex-col border-2 '>
+                        <div className='h-2/3 flex flex-col-reverse rounded-xl bg-white bg-[url("/img/graphic_card.png")] bg-center'>
+                            <div className='grid grid-cols-3 gap-1'>
+                                <div className='col-start-3 font-black'>5<StarIcon sx={{ color: yellow[500] }} /></div>
+                            </div>
+                        </div>
+                        <div className='h-1/3'>
+                            <div className='grid grid-rows-4'>
+                                <div className='row-span-2 text-white font-bold text-xl'>VGA ASUS ROG</div>
+                                <div className='text-oranye font-bold text-lg'>Rp 9.000.000</div>
+                            </div>
+                        </div>
+                    </div>
+                    {/* INI BERULANG */}
+                    <div className='bg-abu-gelap w-48 h-60 rounded-xl flex flex-col border-2 '>
+                        <div className='h-2/3 flex flex-col-reverse rounded-xl bg-white bg-[url("/img/graphic_card.png")] bg-center'>
+                            <div className='grid grid-cols-3 gap-1'>
+                                <div className='col-start-3 font-black'>5<StarIcon sx={{ color: yellow[500] }} /></div>
+                            </div>
+                        </div>
+                        <div className='h-1/3'>
+                            <div className='grid grid-rows-4'>
+                                <div className='row-span-2 text-white font-bold text-xl'>VGA ASUS ROG</div>
+                                <div className='text-oranye font-bold text-lg'>Rp 9.000.000</div>
+                            </div>
+                        </div>
+                    </div>
+                    {/* INI BERULANG */}
+                    <div className='bg-abu-gelap w-48 h-60 rounded-xl flex flex-col border-2 '>
+                        <div className='h-2/3 flex flex-col-reverse rounded-xl bg-white bg-[url("/img/graphic_card.png")] bg-center'>
+                            <div className='grid grid-cols-3 gap-1'>
+                                <div className='col-start-3 font-black'>5<StarIcon sx={{ color: yellow[500] }} /></div>
+                            </div>
+                        </div>
+                        <div className='h-1/3'>
+                            <div className='grid grid-rows-4'>
+                                <div className='row-span-2 text-white font-bold text-xl'>VGA ASUS ROG</div>
+                                <div className='text-oranye font-bold text-lg'>Rp 9.000.000</div>
+                            </div>
+                        </div>
+                    </div>
+                    {/* INI BERULANG */}
+                    <div className='bg-abu-gelap w-48 h-60 rounded-xl flex flex-col border-2 '>
+                        <div className='h-2/3 flex flex-col-reverse rounded-xl bg-white bg-[url("/img/graphic_card.png")] bg-center'>
+                            <div className='grid grid-cols-3 gap-1'>
+                                <div className='col-start-3 font-black'>5<StarIcon sx={{ color: yellow[500] }} /></div>
+                            </div>
+                        </div>
+                        <div className='h-1/3'>
+                            <div className='grid grid-rows-4'>
+                                <div className='row-span-2 text-white font-bold text-xl'>VGA ASUS ROG</div>
+                                <div className='text-oranye font-bold text-lg'>Rp 9.000.000</div>
+                            </div>
+                        </div>
+                    </div>
+                    {/* INI BERULANG */}
+                    <div className='bg-abu-gelap w-48 h-60 rounded-xl flex flex-col border-2 '>
+                        <div className='h-2/3 flex flex-col-reverse rounded-xl bg-white bg-[url("/img/graphic_card.png")] bg-center'>
+                            <div className='grid grid-cols-3 gap-1'>
+                                <div className='col-start-3 font-black'>5<StarIcon sx={{ color: yellow[500] }} /></div>
+                            </div>
+                        </div>
+                        <div className='h-1/3'>
+                            <div className='grid grid-rows-4'>
+                                <div className='row-span-2 text-white font-bold text-xl'>VGA ASUS ROG</div>
+                                <div className='text-oranye font-bold text-lg'>Rp 9.000.000</div>
+                            </div>
+                        </div>
+                    </div>
+                    {/* INI BERULANG */}
+                    <div className='bg-abu-gelap w-48 h-60 rounded-xl flex flex-col border-2 '>
+                        <div className='h-2/3 flex flex-col-reverse rounded-xl bg-white bg-[url("/img/graphic_card.png")] bg-center'>
+                            <div className='grid grid-cols-3 gap-1'>
+                                <div className='col-start-3 font-black'>5<StarIcon sx={{ color: yellow[500] }} /></div>
+                            </div>
+                        </div>
+                        <div className='h-1/3'>
+                            <div className='grid grid-rows-4'>
+                                <div className='row-span-2 text-white font-bold text-xl'>VGA ASUS ROG</div>
+                                <div className='text-oranye font-bold text-lg'>Rp 9.000.000</div>
+                            </div>
+                        </div>
+                    </div>
+                    {/* INI BERULANG */}
+                    <div className='bg-abu-gelap w-48 h-60 rounded-xl flex flex-col border-2 '>
+                        <div className='h-2/3 flex flex-col-reverse rounded-xl bg-white bg-[url("/img/graphic_card.png")] bg-center'>
+                            <div className='grid grid-cols-3 gap-1'>
+                                <div className='col-start-3 font-black'>5<StarIcon sx={{ color: yellow[500] }} /></div>
+                            </div>
+                        </div>
+                        <div className='h-1/3'>
+                            <div className='grid grid-rows-4'>
+                                <div className='row-span-2 text-white font-bold text-xl'>VGA ASUS ROG</div>
+                                <div className='text-oranye font-bold text-lg'>Rp 9.000.000</div>
+                            </div>
+                        </div>
+                    </div>
+                    {/* INI BERULANG */}
                 </div>
             </div>
             <Footer />
