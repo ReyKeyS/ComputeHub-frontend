76441046e44d7bb2cfd43b6ebf10a4d142058b05
RichDH <104489907+RichDH@users.noreply.github.com>
2023-11-09 08:57:24 +0700

    masteruser4

--- src/pageAdmin/MasterUser.jsx
+++ src/pageAdmin/MasterUser.jsx
@@ -15,39 +15,60 @@ function MasterUser() {
               </div>
               
               <div className="listbutton text-white mt-3">
-                <div className="dashboard h-16 flex hover:bg-abu-gelap">
-                  <img></img>
-                  <h2>Dashboard</h2>
+                <div className="dashboard h-16 grid place-items-center justify-items-center hover:bg-abu-gelap">
+                  <div className='flex'>
+                    <img></img>
+                    <h2>Dashboard</h2>
+                  </div>
                 </div>
 
-                <div className="customer h-16 flex hover:bg-abu-gelap">
-                  <img></img>
-                  <h2>Customers</h2>
-                </div>
-                <div className="Master h-16 flex hover:bg-abu-gelap">
-                   <img></img>
-                  <h2>Master Barang</h2>
-                </div>
-                <div className="Report h-16 flex hover:bg-abu-gelap">
-                  <img></img>
-                  <h2>Report</h2>
-                </div>
-                <div className="Promo h-16 flex hover:bg-abu-gelap">
-                  <img></img>
-                  <h2>Promo</h2>
+                <div className="h-16 grid place-items-center justify-items-center hover:bg-abu-gelap">
+                  <div className='flex'>
+                    <img></img>
+                    <h2>Customers</h2>
+                  </div>
                 </div>
 
-                <div className="Confirm h-16 flex hover:bg-abu-gelap">
-                  <img></img>
-                  <h2>Confirm</h2>
+                <div className="h-16 grid place-items-center justify-items-center hover:bg-abu-gelap">
+                  <div className='flex'>
+                    <img></img>
+                    <h2>Master Barang</h2>
+                  </div>
                 </div>
-                <div className="Settings h-16 flex hover:bg-abu-gelap">
-                  <img></img>
-                  <h2>Settings</h2>
+
+                <div className="dashboard h-16 grid place-items-center justify-items-center hover:bg-abu-gelap">
+                  <div className='flex'>
+                    <img></img>
+                    <h2>Report</h2>
+                  </div>
                 </div>
-                <div className="Logout h-16 flex hover:bg-abu-gelap">
-                  <img></img>
-                  <h2>Log Out</h2>
+
+                <div className="h-16 grid place-items-center justify-items-center hover:bg-abu-gelap">
+                  <div className='flex'>
+                    <img></img>
+                    <h2>Promo</h2>
+                  </div>
+                </div>
+
+                <div className="h-16 grid place-items-center justify-items-center hover:bg-abu-gelap">
+                  <div className='flex'>
+                    <img></img>
+                    <h2>Confirm </h2>
+                  </div>
+                </div>
+
+                <div className="h-16 grid place-items-center justify-items-center hover:bg-abu-gelap">
+                  <div className='flex'>
+                    <img></img>
+                    <h2>Settings</h2>
+                  </div>
+                </div>
+
+                <div className="h-16 grid place-items-center justify-items-center hover:bg-abu-gelap">
+                  <div className='flex'>
+                    <img></img>
+                    <h2>Log Out</h2>
+                  </div>
                 </div>
 
               </div>
