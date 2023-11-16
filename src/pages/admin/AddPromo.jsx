2aaa0973c7f9d190a282bc750745dd30f9d61fb6
RichDH <104489907+RichDH@users.noreply.github.com>
2023-11-16 08:09:28 +0700

    promo#1

--- src/pages/admin/AddPromo.jsx
+++ src/pages/admin/AddPromo.jsx
@@ -11,7 +11,7 @@ function AddPromo() {
         <NavbarAdmin />
         <div className="col-span-3 w-full h-screen bg-abu-gelap">
           <HeaderAdmin/>
-          <div className="kananbawah">
+          <div className="">
             <div className="judul text-white text-4xl ms-9 mt-5">
               <h1>Add New Promo</h1>
             </div>
@@ -78,21 +78,21 @@ function AddPromo() {
                 <div className="header flex w-max text-oranye text-2xl">
                     <div className=" w-10 ml-3">No</div>
                     <div className=" w-20 ml-3">ID</div>
-                    <div className=" w-48 ml-3">Name</div>
-                    <div className=" w-56 ml-3">Price</div>
-                    <div className=" w-60 ml-3">Category</div>
-                    <div className=" w-48 ml-3">Amount</div>
+                    <div className=" w-32 ml-3">Name</div>
+                    <div className=" w-48 ml-3">Discount</div>
+                    <div className=" w-52 ml-3">Barang</div>
+                    <div className=" w-80 ml-3">Duration</div>
                     <div className=" w-16 ml-3">Action</div>
                 </div>
 
                 <hr className="bg-oranye"></hr>
                 <div className="listdata flex  text-white h-16 place-items-center text-lg font-bold">
                     <div className="text-align-center w-10 ml-3">1</div>
-                    <div className="text-align-center w-20 ml-3">BRO001</div>
-                    <div className="text-align-center w-48 ml-3">INTIL</div>
-                    <div className="text-align-center w-56 ml-3">Rp 40.000</div>
-                    <div className="text-align-center w-60 ml-3">Processor</div>
-                    <div className="text-align-center w-48 ml-3">7</div>
+                    <div className="text-align-center w-20 ml-3">MEGA01</div>
+                    <div className="text-align-center w-32 ml-3">Mega Sale</div>
+                    <div className="text-align-center w-48 ml-3">50 %</div>
+                    <div className="text-align-center w-52 ml-3">GTX 3080</div>
+                    <div className="text-align-center w-80 ml-3">10 Oktober 2022-10 November 2022</div>
                     <div className="text-align-center w-16 ml-3">...</div>
                     {/* kurang onclick */}
                 </div>
@@ -100,11 +100,11 @@ function AddPromo() {
 
                 <div className="listdata flex text-white h-16 place-items-center text-lg font-bold">
                     <div className="text-align-center w-10 ml-3">2</div>
-                    <div className="text-align-center w-20 ml-3">BRO002</div>
-                    <div className="text-align-center w-48 ml-3">APASE</div>
-                    <div className="text-align-center w-56 ml-3">Rp 500.000</div>
-                    <div className="text-align-center w-60 ml-3">VGA</div>
-                    <div className="text-align-center w-48 ml-3">11</div>
+                    <div className="text-align-center w-20 ml-3">HLW01</div>
+                    <div className="text-align-center w-32 ml-3">Halowen</div>
+                    <div className="text-align-center w-48 ml-3">RP 5.000</div>
+                    <div className="text-align-center w-52 ml-3">GTX 3090</div>
+                    <div className="text-align-center w-80 ml-3">15 Oktober 2022-10 Desember 2022</div>
                     <div className="text-align-center w-16 ml-3">...</div>
                     {/* kurang onclick */}
                 </div>
@@ -113,23 +113,11 @@ function AddPromo() {
 
                 <div className="listdata flex text-white h-16 place-items-center text-lg font-bold">
                     <div className="text-align-center w-10 ml-3">3</div>
-                    <div className="text-align-center w-20 ml-3">BRO003</div>
-                    <div className="text-align-center w-48 ml-3">PRODUK3</div>
-                    <div className="text-align-center w-56 ml-3">Rp 1.240.000</div>
-                    <div className="text-align-center w-60 ml-3">HDD</div>
-                    <div className="text-align-center w-48 ml-3">80</div>
-                    <div className="text-align-center w-16 ml-3">...</div>
-                    {/* kurang onclick */}
-                </div>
-                <hr className="border border-oranye w-11/12 mx-auto"></hr>
-
-                <div className="listdata flex text-white h-16 place-items-center text-lg font-bold">
-                    <div className="text-align-center w-10 ml-3">4</div>
-                    <div className="text-align-center w-20 ml-3">BRO004</div>
-                    <div className="text-align-center w-48 ml-3">Produk4</div>
-                    <div className="text-align-center w-56 ml-3">Rp 10.400.000</div>
-                    <div className="text-align-center w-60 ml-3">VGA</div>
-                    <div className="text-align-center w-48 ml-3">2</div>
+                    <div className="text-align-center w-20 ml-3">DSC01</div>
+                    <div className="text-align-center w-32 ml-3">Dsicount3</div>
+                    <div className="text-align-center w-48 ml-3">RP 50.000</div>
+                    <div className="text-align-center w-52 ml-3">GTX 1090</div>
+                    <div className="text-align-center w-80 ml-3">15 January 2022-10 Desember 2022</div>
                     <div className="text-align-center w-16 ml-3">...</div>
                     {/* kurang onclick */}
                 </div>
