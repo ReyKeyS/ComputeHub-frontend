2a0a3d2ea62cc4ad6f44818bba0dade95fadb94f
RichDH <104489907+RichDH@users.noreply.github.com>
2023-11-27 13:45:06 +0700

    Error message Register update1

--- src/pages/user/RegisterPage.jsx
+++ src/pages/user/RegisterPage.jsx
@@ -11,25 +11,25 @@ function RegisterPage() {
     const navigate = useNavigate();
 
     const schema = Joi.object({
-        name: Joi.string().min(3).required().label("Display_Name").messages({
-            "any.required": "Name is required",
+        name: Joi.string().min(3).required().messages({
+            "string.empty": "Name is required",
             "string.min": "Name must be at least 3 characters",
         }),
-        email: Joi.string().email({ tlds: { allow: false } }).required().label("Email").messages({ 
-            "any.required": "Email is required",
+        email: Joi.string().email({ tlds: { allow: false } }).required().messages({ 
+            "string.empty": "Email is required",
             "string.email": "Email is not a valid email"
         }),
-        address: Joi.string().required().label("Address").messages({
-            "any.required": "Address is required",
+        address: Joi.string().required().messages({
+            "string.empty": "Address is required",
         }),
-        phone_number: Joi.string().required().label("Phone_Number").messages({
-            "any.required": "Phone Number is required",
+        phone_number: Joi.string().required().messages({
+            "string.empty": "Phone Number is required",
         }),
-        password: Joi.string().min(6).required().label("Password").messages({
-            "any.required": "Password is required",
+        password: Joi.string().min(6).required().messages({
+            "string.empty": "Password is required",
             "string.min": "Password must be at least 6 characters",
         }),
-        confirm_password: Joi.any().equal(Joi.ref("password")).label("Konfirmasi Password").messages({ 
+        confirm_password: Joi.any().equal(Joi.ref("password")).messages({ 
             "any.only": "Konfirmasi Password harus sama dengan password" 
         }),
     })
@@ -45,27 +45,42 @@ function RegisterPage() {
     }
     
     return (
-        <div className='w-screen h-screen flex flex-col justify-center bg-gradient-to-r from-white to-black'>
+        <div className='w-screen h-fit flex flex-col justify-center bg-gradient-to-r from-white to-black'>
             <div className='w-1/2 m-auto'>
             <div className='bg-abu-super-gelap text-white rounded-lg text-6xl p-5 w-1/2 m-auto flex justify-center'><ComputerIcon fontSize=''/><div className='mx-2'>Compute</div><a className='bg-oranye px-1 rounded-lg font-bold text-abu-super-gelap pb-1'>HUB</a></div>
                 <div className='bg-abu-super-gelap py-5 px-16 mt-2 w-full m-auto flex flex-col border rounded-3xl place-content-center'>
                     <form onSubmit={handleSubmit(registered)}>
                     <h1 className='text-center font-bold text-4xl text-oranye'>REGISTER</h1>
                     <div className='text-white text-xl mb-1'>Name</div>
-                    <input type="text" placeholder={`${errors.name ? errors?.name?.message : "Enter your name" }`}  {...register('name')} className={`mb-3 px-3 py-2 border shadow rounded-lg w-full h-14 text-xl focus:ring-2 focus:ring-blue-500 focus:outline-none ${errors.name ? "placeholder-red-500" : ""}`} />
+                    <input type="text" placeholder= "Enter your name"   {...register('name')} className={`mb-3 px-3 py-2 border shadow rounded-lg w-full h-14 text-xl focus:ring-2 focus:ring-blue-500 focus:outline-none `} />
+                    <div className='w-4/6 h-4 flex justify-start mt-1'>
+                        <span className='text-red-500 text-xl'>{errors?.name?.message}</span>
+                    </div><br />
                     <div className='text-white text-xl mb-1'>Email</div>
-                    <input type="text"  placeholder={`${errors.email ? errors?.email?.message : "Enter your Email" }`}{...register('email')} className={`mb-3 px-3 py-2 border shadow rounded-lg w-full h-14 text-xl focus:ring-2 focus:ring-blue-500 focus:outline-none ${errors.name ? "placeholder-red-500" : ""}`} />
+                    <input type="text"  placeholder="Enter your Email" {...register('email')} className={`mb-3 px-3 py-2 border shadow rounded-lg w-full h-14 text-xl focus:ring-2 focus:ring-blue-500 focus:outline-none `} />
+                    <div className='w-4/6 h-4 flex justify-start mt-1'>
+                        <span className='text-red-500 text-xl'>{errors?.email?.message}</span>
+                    </div><br />
                     <div className='text-white text-xl mb-1'>Address</div>
-                    <input type="text" placeholder={`${errors.address ? errors?.address?.message : "Enter your Address" }`} {...register('address')} className={`mb-3 px-3 py-2 border shadow rounded-lg w-full h-14 text-xl focus:ring-2 focus:ring-blue-500 focus:outline-none ${errors.name ? "placeholder-red-500" : ""}`}/>
+                    <input type="text" placeholder= "Enter your Address"{...register('address')} className={`mb-3 px-3 py-2 border shadow rounded-lg w-full h-14 text-xl focus:ring-2 focus:ring-blue-500 focus:outline-none `}/>
+                    <div className='w-4/6 h-4 flex justify-start mt-1'>
+                        <span className='text-red-500 text-xl'>{errors?.address?.message}</span>
+                    </div><br />
                     <div className='text-white text-xl mb-1'>Phone Number</div>
-                    <input type="text" placeholder={`${errors.phone_number ? errors?.phone_number?.message : "Enter your Phone Number" }`} {...register('phone_number')} className={`mb-3 px-3 py-2 border shadow rounded-lg w-full h-14 text-xl focus:ring-2 focus:ring-blue-500 focus:outline-none ${errors.name ? "placeholder-red-500" : ""}`} />
+                    <input type="text" placeholder= "Enter your Phone Number" {...register('phone_number')} className={`mb-3 px-3 py-2 border shadow rounded-lg w-full h-14 text-xl focus:ring-2 focus:ring-blue-500 focus:outline-none `} />
+                    <div className='w-4/6 h-4 flex justify-start mt-1'>
+                        <span className='text-red-500 text-xl'>{errors?.phone_number?.message}</span>
+                    </div><br />
                     <div className='text-white text-xl mb-1'>Password</div>
-                    <input type="password" placeholder={`${errors.password ? errors?.password?.message : "Enter your Password" }`}  {...register('password')} className={`mb-3 px-3 py-2 border shadow rounded-lg w-full h-14 text-xl focus:ring-2 focus:ring-blue-500 focus:outline-none ${errors.name ? "placeholder-red-500" : ""}`}/>
+                    <input type="password" placeholder="Enter your Password" {...register('password')} className={`mb-3 px-3 py-2 border shadow rounded-lg w-full h-14 text-xl focus:ring-2 focus:ring-blue-500 focus:outline-none `}/>
+                    <div className='w-4/6 h-4 flex justify-start mt-1'>
+                        <span className='text-red-500 text-xl'>{errors?.password?.message}</span>
+                    </div><br />
                     <div className='text-white text-xl mb-1'>Confirm Password</div>
                     <input type="password" placeholder='Enter your confirm password' {...register('confirm_password')} className={`mb-3 px-3 py-2 border shadow rounded-lg w-full h-14 text-xl focus:ring-2 focus:ring-blue-500 focus:outline-none `} />
                     <div className='w-4/6 h-4 flex justify-start mt-1'>
                         <span className='text-red-500 text-xl'>{errors?.confirm_password?.message}</span>
-                    </div>
+                    </div><br />
 
                     <div className="text-white text-xl text-center mt-3">Already have an account? &nbsp;<Link to="/login"><a className="font-semibold text-oranye">Login Now!</a></Link></div>
                     <div className="flex">
