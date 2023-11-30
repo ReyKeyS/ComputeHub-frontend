ba0d16206e768f2608e73a77d08df874e010439d
lukastjan1 <105804699+lukastjan1@users.noreply.github.com>
2023-12-01 02:02:24 +0700

    Forbidden Page
    401,403,404

---
+++ src/pages/User/EditProfile.jsx
@@ -0,0 +1,6 @@
+import React, { useState, useEffect } from 'react'
+import { useNavigate } from 'react-router-dom'
+import client from "../../services/client"
+
+import Footer from "../../components/Footer"
+import Header from "../../components/Header"
--- src/pages/User/Error.jsx
+++ src/pages/User/Error.jsx
@@ -1,16 +1,45 @@
 import { Link, useRouteError } from "react-router-dom";
+import Footer from "../../components/Footer";
+import Header from "../../components/Header";
 
 const Error = () => {
-  const error = useRouteError()
+  const error = useRouteError();
   return (
-    <div className="flex flex-col w-full h-screen justify-center items-center gap-y-4">
+    <>
+      <Header></Header>
+      <div className="min-h-screen bg-abu-gelap flex items-center justify-center">
+        <div className="bg-abu-super-gelap p-20 rounded-lg text-center">
+          <h1 className="text-5xl font-bold text-oranye">Error!</h1>
 
-    {error.status==401 && <div className="text-xl font-semibold">You aren't authorized to see this</div>}
-    {error.status==403 && <div className="text-xl font-semibold">Forbidden</div>}
-    {error.status==404 && <div className="text-xl font-semibold">404 Page Not Found</div>}
+          
 
-      <Link to="/" className="text-blue-700">Go back</Link>
-    </div>
+
+
+          {error.status === 401 && (
+            <div className="text-9xl font-bold text-oranye">
+            401<br />
+            <p className="text-lg text-white mt-4">You aren't authorized to see this</p>
+          </div>
+          )}
+          {error.status === 403 && (
+            <div className="text-9xl font-bold text-oranye">
+              403<br />
+              <p className="text-lg text-white mt-4">FORBIDDEN</p>
+            </div>
+          )}
+          {error.status === 404 && (
+            <div className="text-9xl font-bold text-oranye">
+              404<br />
+              <p className="text-lg text-white mt-4">Page Not Found, please try again in several minutes</p>
+            </div>
+          )}
+
+          <Link to="/" className="text-oranye">
+            Go back
+          </Link>
+        </div>
+      </div>
+    </>
   );
 };
 
