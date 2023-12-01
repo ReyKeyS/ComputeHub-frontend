d06711d8e790ed07abe32b12030d479502d12bcb
ReyKeyS <104489667+ReyKeyS@users.noreply.github.com>
2023-12-01 12:30:10 +0700

    Chat complete..

--- src/pages/Admin/ChatAdmin.jsx
+++ src/pages/Admin/ChatAdmin.jsx
@@ -1,4 +1,9 @@
-import * as React from 'react';
+import React, { useState, useEffect, useRef} from 'react';
+import { useNavigate } from 'react-router-dom'
+import { useForm } from "react-hook-form";
+import client from "../../services/client"
+
+
 import Header from '../../components/Header';
 import NavbarAdmin from '../../components/NavbarAdmin';
 import HeaderAdmin from '../../components/HeaderAdmin';
@@ -9,6 +14,41 @@ import SearchIcon from '@mui/icons-material/Search';
 import SendIcon from '@mui/icons-material/Send';
 
 function ChatAdmin() {
+    const ref = useRef(null)
+    const navigate = useNavigate()
+    const [user, setUser] = useState();
+    const [whosChat, setWhosChat] = useState();
+    const [search, setSearch] = useState("");
+
+    useEffect(() => {
+        if (!localStorage.getItem("user_token")) navigate("/")
+
+        client.get("/users/detail", {
+            headers: { "Authorization": "Bearer " + localStorage.getItem("user_token")}
+        }).then((res)=>{
+            console.log(res.data);
+            setUser(res.data)
+        }).catch((err) => {console.log(err)});
+    }, [])
+
+    useEffect(() =>{
+        if (ref.current)
+          ref.current.scrollTop = ref.current.scrollHeight;
+    }, [user])
+
+    const { register, handleSubmit } = useForm();
+    
+    const addChat = (data) => {
+        client.post("/users/chat/add", {
+            content: data.content,
+            email_to: whosChat.email_sender
+        },{
+            headers: { "Authorization": "Bearer " + localStorage.getItem("user_token")}
+        }).then((res) => {
+            navigate(0)
+        }).catch((err) => {console.log(err)});
+    }
+
     return (<div className='w-full h-[calc(100vh-6rem)]'>
         <div className="judul text-white text-4xl ms-12 py-5">
             <h1>Chat</h1>
@@ -16,43 +56,72 @@ function ChatAdmin() {
         <div className="grid grid-cols-3 gap-4 ms-4">
             <div className="h-[calc(100vh-12rem)] rounded col-span-1 border border-oranye">
                 <div className="h-[6rem] w-full flex items-center justify-center text-oranye border-b border-oranye">
-                    <input className= "w-5/6 px-4 py-2 text-white text-xl bg-abu-gelap border border-oranye rounded-xl" placeholder="Search.." />
+                    <input className= "w-5/6 px-4 py-2 text-white text-xl bg-abu-gelap border border-oranye rounded-xl" placeholder="Search.." onChange={(e)=>{setSearch(e.target.value)}}/>
                 </div>
 
                 <div className="w-full h-[calc(100vh-18.1rem)]">
-
                     <div className="w-full h-[calc(100vh-18.1rem)] bg-abu-super-gelap text-white file:overflow-y-auto no-scrollbar">
                         
-                        <div className="h-24 grid place-items-center justify-items-center hover:border-x-4 border-oranye">
-                            <div className="h-24 w-full grid place-items-center justify-items-center hover:bg-abu-gelap  ">
-                                <div className='flex'>
-                                    <img className="bg-white w-14 h-14 mr-16 rounded-full"></img>
-                                    <div className="text-white mr-10">
-                                        <h2 className="text-xl">My Son</h2>
-                                        <p>Cocok buat oek hantam</p>
+                        {user?.chats?.sort((a, b)=>{
+                            if (a.latest_time != null && b.latest_time != null) 
+                                return new Date(b.latest_time) - new Date(a.latest_time)
+                            else return 0
+                        }).filter((f) => f.name_sender.toLowerCase().includes(search.toLowerCase()))
+                        .map((c) => {
+                            return (<>
+                                <div className={"w-full h-28 grid place-items-center justify-items-center border-oranye hover:cursor-pointer"+(c==whosChat?" border-x-4 bg-abu-gelap":" hover:border-x-4 hover:bg-abu-gelap")} onClick={()=>{setWhosChat(c)}}>
+                                    <div className="h-28 w-full grid place-items-center justify-items-center">
+                                        <div className='flex'>
+                                            <img src={import.meta.env.VITE_BACKEND_GET_PICTURE_URL+c.profpict_sender} className="w-14 h-14 rounded-full"></img>
+                                            <div className="w-[14rem] text-white ms-10">
+                                                <h2 className="text-2xl font-bold truncate">{c.name_sender}</h2>
+                                                <p className='text-lg truncate'>{c.latest_chat}</p>
+                                            </div>
+                                        </div>
                                     </div>
                                 </div>
-                            </div>
-                        </div>
+                            </>)
+                        })}
 
                     </div>
                 </div>
             </div>
 
             <div className="h-[calc(100vh-12rem)] rounded flex flex-col col-span-2 border border-oranye me-4">
-                <div className='h-[6rem] bg-abu-gelap border-b-2 border-oranye shadow-2xl w-full text-white text-4xl flex '>
-                    <div className='flex ml-4 items-center px-3'>
-                        <Avatar src={import.meta.env.VITE_BACKEND_GET_PICTURE_URL+"/profile_default.png"} />
-                        <span className='font-bold ms-6'>My Son</span>
+                {!whosChat && 
+                    <div className='w-full h-full flex items-center justify-center'>
+                        <div className='w-1/2 text-center text-white text-6xl'>Select the Customer to Reply the Chat</div>
                     </div>
-                </div>
-                <div className='h-[calc(100vh-24rem)]'>
-                    
-                </div>
-                <div className="h-[6rem] flex items-center justify-between px-10 border-t border-oranye">
-                    <input className="text-white text-xl px-4 py-2 bg-abu-gelap border border-oranye w-full rounded-s-xl" placeholder='Type Here..' />
-                    <button className='text-white text-lg border-y border-e bg-abu-gelap border-oranye px-4 py-2 rounded-e-xl'><SendIcon /></button>
-                </div>
+                }
+                {whosChat && 
+                    <>
+                        <div className='h-[6rem] bg-abu-gelap border-b-2 border-oranye shadow-2xl w-full text-white text-4xl flex '>
+                            <div className='flex ml-4 items-center px-3'>
+                                <Avatar src={import.meta.env.VITE_BACKEND_GET_PICTURE_URL+whosChat?.profpict_sender} />
+                                <span className='font-bold ms-6'>{whosChat?.name_sender}</span>
+                            </div>
+                        </div>
+                        <div ref={ref} className='w-full h-[calc(100vh-24rem)] bg-neutral-200 p-2 overflow-y-auto no-scrollbar'>
+                            {whosChat?.chatting?.map((c, index) => {
+                            return (<>
+                                <div className={'chat ' + (user?.email == c.sender?" chat-end" : " chat-start")}>
+                                    {/* <div className='chat-header'>{c.sender}</div> */}
+                                    <div className={'chat-bubble bg-abu-super-gelap break-words text-2xl'+(user?.email == c.sender?"" : " bg-oranye text-black")}>
+                                        <div className='px-2 py-1'>{c.content}</div>
+                                    </div>
+                                    <div className='chat-footer text-lg font-bold'>{new Date(c.time).toLocaleTimeString("id-ID").substring(0, 5)}</div>
+                                </div>
+                            </>)
+                        })}
+                        </div>
+                        <form onSubmit={handleSubmit(addChat)}>
+                            <div className="h-[6rem] flex items-center justify-between px-10 border-t border-oranye">
+                                <input className="text-white text-xl px-4 py-2 bg-abu-gelap border border-oranye w-full rounded-s-xl" placeholder='Type Here..' {...register('content')} />
+                                <button type='submit' className='text-white text-lg border-y border-e bg-abu-gelap hover:bg-oranye transition duration-300 border-oranye px-4 py-2 rounded-e-xl'><SendIcon /></button>
+                            </div>
+                        </form>
+                    </>
+                }
             </div>
         </div>
     </div>)
--- src/pages/User/ChatUser.jsx
+++ src/pages/User/ChatUser.jsx
@@ -1,4 +1,5 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState, useEffect, useRef } from 'react'
+import { useForm } from "react-hook-form";
 import { useNavigate } from 'react-router-dom'
 import client from "../../services/client"
 
@@ -7,35 +8,70 @@ import Header from "../../components/Header"
 
 // Material UI
 import { TextField } from '@mui/material'
+import SendIcon from '@mui/icons-material/Send';
 
 const ChatUser = () => {
-    const navigate = useNavigate()
+    const ref = useRef(null);
+    const navigate = useNavigate();
+    const [user, setUser] = useState();
+    const [userChat, setUserChat] = useState();
 
     useEffect(() => {
         if (!localStorage.getItem("user_token")) navigate("/")
+
+        client.get("/users/detail", {
+            headers: { "Authorization": "Bearer " + localStorage.getItem("user_token")}
+        }).then((res)=>{
+            setUser(res.data)
+            setUserChat(res.data.chats[0]);
+        }).catch((err) => {console.log(err)});
     }, [])
+
+    useEffect(() =>{
+        if (ref.current)
+          ref.current.scrollTop = ref.current.scrollHeight;
+    }, [userChat])
+
+    const { register, handleSubmit } = useForm();
     
+    const addChat = (data) => {
+        client.post("/users/chat/add", {
+            content: data.content,
+            email_to: "admin@gmail.com"
+        },{
+            headers: { "Authorization": "Bearer " + localStorage.getItem("user_token")}
+        }).then((res) => {
+            navigate(0)
+        }).catch((err) => {console.log(err)});
+    }
+
     return (<>
         <Header />
-        <div className='min-h-[calc(100vh-5rem)] flex justify-center p-10'>
+        <div className='min-h-[calc(100vh-5rem)] flex justify-center p-10 bg-neutral-700'>
             <div className='w-3/5 bg-abu-super-gelap rounded-2xl px-8 py-6'>
                 <div className='flex pb-5 border-b-2 border-oranye'>
                     <img src="/img/istts_bg.png" alt="" className='w-12 h-12'/>
-                    <p className='text-oranye text-4xl mt-1 ms-5'>Chat Admin</p>
+                    <p className='text-oranye text-4xl mt-1 ms-5'>Chat dengan Admin</p>
                 </div>
-                <div className='w-full min-h-[calc(100vh-23rem)] bg-abu-abu overflow-y-auto'>
-                    {/* isi Chat */}
-                    <div className="chat chat-start">
-                        <div className="chat-bubble bg-oranye">It's over Anakin, <br/>I have the high ground.</div>
+                <div ref={ref} className='w-full h-[calc(100vh-23rem)] bg-neutral-200 p-2 overflow-y-auto no-scrollbar'>
+                    {userChat?.chatting?.map((c, index) => {
+                        return (<>
+                            <div className={'chat ' + (user?.email == c.sender?" chat-end" : " chat-start")}>
+                                {/* <div className='chat-header'>{c.sender}</div> */}
+                                <div className={'chat-bubble bg-abu-super-gelap break-words text-2xl'+(user?.email == c.sender?"" : " bg-oranye text-black")}>
+                                    <div className='px-2 py-1'>{c.content}</div>
+                                </div>
+                                <div className='chat-footer text-lg font-bold'>{new Date(c.time).toLocaleTimeString("id-ID").substring(0, 5)}</div>
+                            </div>
+                        </>)
+                    })}
+                </div>
+                <form onSubmit={handleSubmit(addChat)}>
+                    <div className="h-[6rem] flex items-center justify-between px-10 pt-5 border-t-2 border-oranye">
+                        <input autoFocus className="text-white text-xl px-7 py-3 bg-abu-gelap border border-oranye w-full rounded-s-xl" placeholder='Type Here..' {...register("content")}/>
+                        <button type='submit' className='text-white text-lg border-y border-e bg-abu-gelap hover:bg-oranye transition duration-300 border-oranye px-4 py-3 rounded-e-xl'><SendIcon /></button>
                     </div>
-                    <div className="chat chat-end">
-                        <div className="chat-bubble">You underestimate my power!</div>
-                    </div>
-                </div>
-                <div className='w-full flex border-t-2 border-oranye pt-7'>
-                    <input type="text" className='w-full px-7 py-3 rounded-xl text-xl text-white bg-abu-gelap' placeholder='Type here...'/>
-                    <button className='px-12 py-2 ms-5 rounded-xl bg-oranye hover:bg-hover-oranye transition duration-300 text-2xl tracking-wide text-white font-bold'>Send</button>
-                </div>
+                </form>
             </div>
         </div>
         <Footer />
