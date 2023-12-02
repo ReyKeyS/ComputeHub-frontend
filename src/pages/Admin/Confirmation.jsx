fcae69a4ae0facb2a98b5dd6889aea65d0e5a00c
ReyKeyS <rkohans@gmail.com>
2023-12-02 22:41:16 +0700

    tampilan confirmation integrated

--- src/pages/Admin/Confirmation.jsx
+++ src/pages/Admin/Confirmation.jsx
@@ -10,64 +10,84 @@ function Confirmation() {
         client.get("/users/transaction/fetch", {
             headers: {"Authorization": "Bearer " + localStorage.getItem("user_token")},
         }).then((res) => {
-            console.log(res.data);
+            const pendingTrans = []
+            for (const t of res.data) {
+                if (t.status == 2) pendingTrans.push(t)
+            }
+            setListTrans(pendingTrans);
         }).catch((err) => {console.log(err)});
     }, [])
 
-    return (<div className='w-full h-[calc(100vh-6rem)]'>
+    const rejectTrans = (id) => {
+
+    }
+
+    const confirmTrans = (id) => {
+        
+    }
+
+    return (<div className='w-full min-h-[calc(100vh-6rem)]'>
         <div className="judul text-white text-5xl font-bold ms-10 my-7">
             <h1>Confirmation</h1>
         </div>
 
-        <div className='text-center mx-10'>
-
-            <div className="rounded-lg w-full mt-4 h-80 text-white border border-oranye bg-abu-gelap">
-                <div className="header flex text-2xl">
-                    <div className="w-48 ml-3 text-oranye">Pesanan Baru</div>
-                    <div className="w-32 ml-3 text-oranye">INV003</div>
-                    <div className="w-44 ml-3 text-white">BUDI AGUNG</div>
-                    <div className="w-64 ml-3 text-white">31 Agustus 2023 </div>
-                    <div className="w-32 text-align-left text-white">19:50 WIB</div>
-                    {/* <div className=" w-16 ml-3">Action</div> */}
+        <div className='text-center h-auto mx-10 mb-36'>
+            {listTrans.length == 0 && 
+                <div className='w-full h-[calc(100vh-18rem)] flex items-center justify-center'>
+                    <div className='text-6xl text-white font-bold'>You have no confirmations at this time</div>
                 </div>
+            }
 
-                <hr className="border border-oranye"></hr>
-                <div className="listdata flex text-white h-40 place-items-center text-lg font-bold">
-                    <div className="text-align-center bg-oranye w-40 h-32 mt-5 ml-8 rounded">
-                        <img src="" alt="" />
-                    </div>
-                    <div className="text-align-center w-52 h-32 mt-5 ml-8">
-                        <div className="ml-3 mt-3 text-2xl text-white">RTX 4090 TI</div>
-                        <div className="ml-3 mt-3 text-xl text-white">1 x Rp. 40.000.000</div>
-                    </div>
-                    <div className="text-align-center w-64 h-32 mt-5 ml-10">
-                        <div className="ml-3 mt-3 text-2xl text-white">Address</div>
-                        <div className="ml-3 mt-3  text-white">Jl Tongkrongan sok asik 87, Surabaya</div>
-                    </div>
+            {listTrans && listTrans.map((t, index) => {
+                console.log(t)
+                return (
+                    <div className="rounded-xl w-full my-8 h-auto text-white border border-oranye bg-abu-gelap" key={index}>
+                        <div className="flex py-2 text-3xl">
+                            <div className="w-52 text-oranye">Pesanan Baru</div>
+                            <div className="w-64 mx-2 text-oranye">{t.invoice}</div>
+                            <div className="w-[25rem] truncate mx-2 text-start text-white">{t.user_id.display_name}</div>
+                            <div className="w-[17.5rem] mx-1 text-white">{new Date(t.payment_date).toLocaleDateString("id-ID", {year: 'numeric', month: 'long', day: 'numeric'})}</div>
+                            <div className="w-36 text-white">{new Date(t.payment_date).toLocaleDateString("id-ID", {hour: 'numeric', minute: 'numeric'}).substring(11)} WIB</div>
+                        </div>
 
-                </div>
-                <div className="flex mt-10 ml-7">
-                    <div className="text-2xl flex">
-                        <div className="text-white">
-                            Transaction Amount : 
+                        <hr className="border border-oranye"></hr>
+                        <div className="w-full h-auto text-white text-lg font-bold">
+                            
+                            {t.detail_trans && t.detail_trans.map((d, index) => {
+                                return (<div className='flex items-center px-10 py-3 border-dashed border-b border-oranye last-of-type:border-solid' key={index}>
+                                    <img src={import.meta.env.VITE_BACKEND_GET_PICTURE_URL+d.item_id.picture} className='w-32 h-32 rounded-lg mx-10'/>
+                                    <div className="text-start w-full h-32 mt-5 ml-8">
+                                        <div className="ml-3 mt-3 text-3xl text-white truncate">{d.item_id.name}</div>
+                                        <div className="ml-3 mt-3 text-2xl text-white">{d.qty} x Rp {d.price.toLocaleString("id-ID")}</div>
+                                    </div>
+                                </div>)
+                            })}
+
                         </div>
-                        <div className="text-oranye ml-3">
-                            Rp. 40.000.000 
+                        <div className="flex w-full h-16 px-10 items-center">
+                            <div className="text-3xl flex w-3/5">
+                                <div className="text-white">
+                                    Transaction Amount : 
+                                </div>
+                                <div className="text-oranye ml-3">
+                                    Rp {t.grand_total.toLocaleString("id-ID")}
+                                </div>
+                            </div>
+                            <div className="flex items-center justify-end gap-12 w-2/5 font-bold text-2xl">
+                                <div>
+                                    <button className='rounded-xl bg-oranye hover:bg-hover-oranye transition duration-300 text-black w-48 h-10' onClick={()=>rejectTrans(t._id)}>Reject</button>
+                                </div>
+                                <div>
+                                    <button className='rounded-xl bg-oranye hover:bg-hover-oranye transition duration-300 text-black w-48 h-10' onClick={()=>confirmTrans(t._id)}>Confirm</button>
+                                </div>
+                            </div>
                         </div>
                     </div>
-                    <div className="flex justify-end ml-auto">
-                        <div className="col-span-4 text-right mr-12">
-                            <button className='rounded bg-oranye text-black mt-5 w-48 h-10 text-xl '>Batal</button>
-                        </div>
-                        <div className="col-span-4 text-right mr-12">
-                            <button className='rounded bg-oranye text-black mt-5 w-48 h-10 text-xl '>Terima</button>
-                        </div>
-                    </div>
-                </div>
-            </div>
+                )
+            })}
+            
         </div>
 
-        {/* ulangi terus */}
     </div>)
 }
 
