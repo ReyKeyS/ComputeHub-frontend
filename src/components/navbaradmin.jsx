ebf4cdb2039f7258d559ef36d3828f7f98484cd7
PaddyHermanito <paddy.hermanito.suhartanto93@gmail.com>
2023-12-03 14:24:22 +0700

    Update navbaradmin.jsx

--- src/components/navbaradmin.jsx
+++ src/components/navbaradmin.jsx
@@ -27,42 +27,42 @@ function NavbarAdmin() {
                 </div>
 
                 <div className="listbutton text-white mt-14">
-                    <NavLink to="/admin" className={(state) => `h-24 flex items-center justify-center justify-items-center hover:border-s-4 hover:border-e-4 hover:bg-abu-gelap border-oranye ${state.isActive ? "bg-abu-gelap border-x-4 border-oranye" : ""}`}>
+                    <NavLink to="/admin" className={(state) => `h-24 flex items-center justify-center justify-items-center hover:border-x-4 hover:bg-abu-gelap border-oranye ${state.isActive ? "bg-abu-gelap border-x-4 border-oranye" : ""}`}>
                         <HomeIcon fontSize='large' className="me-3" />
                         <p className='text-3xl'>Dashboard</p>
                     </NavLink>
 
-                    <NavLink to="/admin/masteruser" className={(state) => `h-24 flex items-center justify-center justify-items-center hover:border-s-4 hover:border-e-4 hover:bg-abu-gelap border-oranye ${state.isActive ? "bg-abu-gelap border-x-4 border-oranye" : ""}`}>
+                    <NavLink to="/admin/masteruser" className={(state) => `h-24 flex items-center justify-center justify-items-center hover:border-x-4 hover:bg-abu-gelap border-oranye ${state.isActive ? "bg-abu-gelap border-x-4 border-oranye" : ""}`}>
                         <HomeIcon fontSize='large' className="me-3" />
                         <p className='text-3xl'>Customers</p>
                     </NavLink>
 
-                    <NavLink to="/admin/masteritem" className={(state) => `h-24 flex items-center justify-center justify-items-center hover:border-s-4 hover:border-e-4 hover:bg-abu-gelap border-oranye ${state.isActive ? "bg-abu-gelap border-x-4 border-oranye" : ""}`}>
+                    <NavLink to="/admin/masteritem" className={(state) => `h-24 flex items-center justify-center justify-items-center hover:border-x-4 hover:bg-abu-gelap border-oranye ${state.isActive ? "bg-abu-gelap border-x-4 border-oranye" : ""}`}>
                         <InventoryIcon fontSize='large' className="me-3" />
                         <p className='text-3xl'>Master Item</p>
                     </NavLink>
 
-                    <NavLink to="/admin/report" className={(state) => `h-24 flex items-center justify-center justify-items-center hover:border-s-4 hover:border-e-4 hover:bg-abu-gelap border-oranye ${state.isActive ? "bg-abu-gelap border-x-4 border-oranye" : ""}`}>
+                    <NavLink to="/admin/report" className={(state) => `h-24 flex items-center justify-center justify-items-center hover:border-x-4 hover:bg-abu-gelap border-oranye ${state.isActive ? "bg-abu-gelap border-x-4 border-oranye" : ""}`}>
                         <SummarizeIcon fontSize='large' className="me-3" />
                         <p className='text-3xl'>Report</p>
                     </NavLink>
 
-                    <NavLink to="/admin/promo" className={(state) => `h-24 flex items-center justify-center justify-items-center hover:border-s-4 hover:border-e-4 hover:bg-abu-gelap border-oranye ${state.isActive ? "bg-abu-gelap border-x-4 border-oranye" : ""}`}>
+                    <NavLink to="/admin/promo" className={(state) => `h-24 flex items-center justify-center justify-items-center hover:border-x-4 hover:bg-abu-gelap border-oranye ${state.isActive ? "bg-abu-gelap border-x-4 border-oranye" : ""}`}>
                         <DiscountIcon fontSize='large' className="me-3" />
                         <p className='text-3xl'>Promo</p>
                     </NavLink>
 
-                    <NavLink to="/admin/confirmation" className={(state) => `h-24 flex items-center justify-center justify-items-center hover:border-s-4 hover:border-e-4 hover:bg-abu-gelap border-oranye ${state.isActive ? "bg-abu-gelap border-x-4 border-oranye" : ""}`}>
+                    <NavLink to="/admin/confirmation" className={(state) => `h-24 flex items-center justify-center justify-items-center hover:border-x-4 hover:bg-abu-gelap border-oranye ${state.isActive ? "bg-abu-gelap border-x-4 border-oranye" : ""}`}>
                         <CheckIcon fontSize='large' className="me-3" />
                         <p className='text-3xl'>Confirmation</p>
                     </NavLink>
 
-                    <NavLink to="/admin/chat" className={(state) => `h-24 flex items-center justify-center justify-items-center hover:border-s-4 hover:border-e-4 hover:bg-abu-gelap border-oranye ${state.isActive ? "bg-abu-gelap border-x-4 border-oranye" : ""}`}>
+                    <NavLink to="/admin/chat" className={(state) => `h-24 flex items-center justify-center justify-items-center hover:border-x-4 hover:bg-abu-gelap border-oranye ${state.isActive ? "bg-abu-gelap border-x-4 border-oranye" : ""}`}>
                         <ChatBubbleIcon fontSize='large' className="me-3" />
                         <p className='text-3xl'>Chat</p>
                     </NavLink>
 
-                    <NavLink to="/admin/logout" className={(state) => `h-24 flex items-center justify-center justify-items-center hover:border-s-4 hover:border-e-4 hover:bg-abu-gelap border-oranye ${state.isActive ? "bg-abu-gelap border-x-4 border-oranye" : ""}`}>
+                    <NavLink to="/admin/logout" className={(state) => `h-24 flex items-center justify-center justify-items-center hover:border-x-4 hover:bg-abu-gelap border-oranye ${state.isActive ? "bg-abu-gelap border-x-4 border-oranye" : ""}`}>
                         <LogoutIcon fontSize='large' className="me-3" />
                         <p className='text-3xl'>Logout</p>
                     </NavLink>
