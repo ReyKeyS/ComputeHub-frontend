04f5b60743745e86ac5bc336738099ab3ee76942
RichDH <104489907+RichDH@users.noreply.github.com>
2023-12-05 14:08:31 +0700

    rubah nama file, nambah history

--- src/pages/User/History.jsx
+++ src/pages/User/History.jsx
@@ -11,40 +11,30 @@ function History() {
   const [listHistory, setListHistory] = useState([])
 
   useEffect(() => {
-    client.get("users/transaction/fetch", {
-        headers: { "Authorization": "Bearer " + localStorage.getItem("user_token") },
-    }).then((res)=>{
-      setListHistory(res.data)
-    }).catch((err) => {
-      console.log(err);
-    })
+    if (localStorage.getItem('user_token')) {
+        client.get('/users/transaction/history', {
+            headers: { "Authorization": "Bearer " + localStorage.getItem("user_token") },
+            params: { userId: userId }
+        }).then((res) => {
+            setListHistory(res.data)
+            console.log(res.data);
+        }).catch((err) => {
+            console.log(err);
+        })
+    }
   }, [])
 
   return (
     <>
         <span className="text-6xl font-bold">History</span> <br />
-            <div className="grid grid-cols-2 gap-4 h-full">
-                <div className="border border-oranye rounded-3xl h-1/2 p-8 font-black text-5xl flex flex-col">
+            <div className="grid grid-cols-2 gap-4 h-auto">
+                <div className="border border-oranye rounded-3xl h-fit p-8 font-black text-5xl flex flex-col">
                     <div className="flex flex-col">
                         <div>
                             <span className=""> </span>
                             <span className="text-oranye">Produk</span>
                         </div>
                         <span className="text-xl">In Your Cart</span>
-                    </div>
-                </div>
-                <div className="border border-oranye rounded-3xl h-1/2 p-8 font-black text-5xl grid grid-rows-2">
-                    <div className="flex flex-col">
-                        <div>
-                            <span className=""> </span>
-                            <span className="text-oranye">Produk</span>
-                        </div>
-                        <span className="text-xl">Ordered</span>
-                    </div>
-                    <div className="self-end">
-                        <button className="bg-oranye px-3 py-2 rounded-xl text-abu-super-gelap"
-                        onClick={async (e) => {navigate("/profile/history")}}>History</button>
-                       
                     </div>
                 </div>
             </div>
--- src/pages/admin/Dashboard.jsx
+++ src/pages/admin/Dashboard.jsx
@@ -1,6 +1,6 @@
 import * as React from 'react';
 import Header from '../../components/Header';
-import NavbarAdmin from '../../components/navbaradmin';
+import NavbarAdmin from '../../components/NavbarAdmin';
 import HeaderAdmin from '../../components/HeaderAdmin';
 
 function Dashboard() {
