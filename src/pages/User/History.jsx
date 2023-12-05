e3043e0427a3c3494e8327978839da575f7774f7
RichDH <104489907+RichDH@users.noreply.github.com>
2023-12-06 00:57:52 +0700

    Progress History

--- src/pages/User/History.jsx
+++ src/pages/User/History.jsx
@@ -5,11 +5,29 @@ import client from '../../services/client'
 import Header from '../../components/Header'
 import Footer from '../../components/Footer';
 
+//MUI
+import HistoryToggleOffIcon from '@mui/icons-material/HistoryToggleOff';
+import CheckIcon from '@mui/icons-material/Check';
+import CloseIcon from '@mui/icons-material/Close';
+
 
 function History() {
     const navigate = useNavigate();
     const [user, setUser] = useState()
     const [listHistory, setListHistory] = useState([])
+    const [listItemHistory, setListItemHistory] = useState([])
+    const [jam, setJam] = useState()
+    const [tanggal, setTanggal] = useState()
+
+    function formatTanggalWaktu(tanggalWaktuISO) {
+        const tanggalWaktu = new Date(tanggalWaktuISO);
+      
+        const tanggal = tanggalWaktu.toLocaleDateString('id-ID');
+        const jam = tanggalWaktu.toLocaleTimeString('id-ID');
+      
+        return (<div>{tanggal} | {jam}</div>);
+      }
+      
 
     useEffect(() => {
         if (localStorage.getItem('user_token')) {
@@ -31,7 +49,8 @@ function History() {
                 headers: { "Authorization": "Bearer " + localStorage.getItem("user_token") },
             }).then((res) => {
                 setListHistory(res.data)
-                console.log("res.data", res.data);
+                console.log( res.data);
+                console.log( res.data[0].detail_trans[0].item_id.picture);
             }).catch((err) => {
                 console.log(err);
             })
@@ -42,38 +61,47 @@ function History() {
         <>
             <span className="text-6xl font-bold">History</span> <br />
             <div className="h-auto">
-                {/* listHistory.map */}
-                <div className="border border-oranye rounded-3xl h-fit font-black ">
-                    <div className="grid grid-cols-3">
-                        <div className="">
-                            <div className="ml-8 mb-1 mt-8 text-2xl">INV </div>
-                            <div className="ml-8 mb-2  text-2xl">Tanggal </div>
-                        </div>
-                        <div className="col-span-2">
-                            <div className="text-end mb-1 mt-8 mr-4 text-4xl">Status </div>
-                        </div>
-                    </div>
-                    <div className=" w-5/6 m-auto rounded-3xl mb-8 bg-abu-super-gelap h-56 grid grid-cols-4 ">
-                        <div className="">
-                            <img src="" alt="" />
-                        </div>
-                        <div className="text-2xl">
-                            <div className="ml-8 mb-2 mt-8 text-2xl">Nama </div>
-                            <div className="ml-8 mb-2  text-2xl">Harga </div>
-                        </div>
-                        <div className="col-span-2">
-                            <div className="text-end text-oranye mt-4 mr-20 text-4xl">
-                                Total
+            {listHistory?.map((item, index) => (
+                 <div className="mt-8" key={index}>
+                    <div className="border border-oranye rounded-3xl h-fit font-black ">
+                        <div className="grid grid-cols-3">
+                            <div className="">
+                                <div className="ml-8 mb-1 mt-8 text-2xl">{item.invoice} </div>
+                                <div className="ml-8 mb-2  text-2xl">{formatTanggalWaktu(item.trans_date)} </div>
                             </div>
-                            <div className="text-end mr-20 text-4xl text-white">
-                                Rp 36.000.000
-                            </div>
-                            <div className="text-end">
-                                <button className=" bg-oranye px-3 py-2 mr-4 mt-4 w-48 h-12 rounded-xl text-2xl mr-20 text-abu-super-gelap">Detail</button>
+                            <div className="col-span-2">
+                                <div className="text-end mb-1 mt-8 mr-4 text-4xl">
+                                    {item.status === 1 && <div className='text-green-400'>Approved <CheckIcon></CheckIcon></div>}
+                                    {item.status === 2 && <div className='text-oranye'>Pending <HistoryToggleOffIcon></HistoryToggleOffIcon></div>}
+                                    {item.status !== 1 && item.status !== 2 && <div className='text-red-500'>Rejected <CloseIcon></CloseIcon></div>}
+                                </div>
                             </div>
                         </div>
+                        {item.detail_trans?.map((detail, index) => (
+                        <div className=" w-5/6 m-auto rounded-3xl mb-8 bg-abu-super-gelap h-56 grid grid-cols-4 "key={index}>
+                            <div className="w-20 h-20 rounded">
+                                <img src={import.meta.env.VITE_BACKEND_GET_PICTURE_URL+detail.item_id.picture} alt="Item" />
+                            </div>
+                            <div className="text-2xl">
+                                <div className="ml-8 mb-2 mt-8 text-2xl">Nama </div>
+                                <div className="ml-8 mb-2  text-2xl">Harga </div>
+                            </div>
+                            <div className="col-span-2">
+                                <div className="text-end text-oranye mt-4 mr-20 text-4xl">
+                                    Total
+                                </div>
+                                <div className="text-end mr-20 text-4xl text-white">
+                                    Rp 36.000.000
+                                </div>
+                                <div className="text-end">
+                                    <button className=" bg-oranye px-3 py-2 mr-4 mt-4 w-48 h-12 rounded-xl text-2xl mr-20 text-abu-super-gelap">Detail</button>
+                                </div>
+                            </div>
+                        </div>
+                         ))}
                     </div>
                 </div>
+            ))}
             </div>
         </>
     )
