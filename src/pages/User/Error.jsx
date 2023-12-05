83f39653dca44643352691a9e6495908809ee459
ReyKeyS <104489667+ReyKeyS@users.noreply.github.com>
2023-12-05 17:25:03 +0700

    perbagus error

--- src/pages/User/Error.jsx
+++ src/pages/User/Error.jsx
@@ -6,12 +6,14 @@ const Error = () => {
   const error = useRouteError();
   return (
     <>
-      <Header></Header>
-      <div className="min-h-screen bg-abu-gelap flex items-center justify-center">
-        <div className="bg-abu-super-gelap p-20 rounded-lg text-center">
+      <Header />
+      <div className="h-[calc(100vh-5rem)] bg-abu-gelap flex items-center justify-center relative">
+        <div className="absolute top-50 left-50 z-0 text-oranye text-[50rem] opacity-50">
+          {error.status}
+        </div>
+        <div className="bg-abu-super-gelap px-20 pt-20 pb-10 rounded-lg text-center z-10 bg-opacity-80">
           <h1 className="text-5xl font-bold text-oranye">Error!</h1>
 
-          
 
 
 
@@ -34,11 +36,12 @@ const Error = () => {
             </div>
           )}
 
-          <Link to="/" className="text-oranye">
-            Go back
+          <Link to="/" className="text-oranye text-4xl flex justify-center">
+            <div className="w-1/2 mt-10 bg-abu-super-gelap border border-oranye hover:bg-black transition duration-500 rounded-xl py-2">Go back</div>
           </Link>
         </div>
       </div>
+      <Footer />
     </>
   );
 };
