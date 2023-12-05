e8d1a0ea2ffb2340a29c5f15bf5f14c61066ba99
PaddyHermanito <paddy.hermanito.suhartanto93@gmail.com>
2023-12-06 00:58:09 +0700

    ganti card shop

--- src/pages/User/Shop.jsx
+++ src/pages/User/Shop.jsx
@@ -14,6 +14,11 @@ import Rating from '@mui/material/Rating';
 import Stack from '@mui/material/Stack';
 import StarIcon from '@mui/icons-material/Star';
 import { yellow } from '@mui/material/colors';
+import Card from '@mui/material/Card';
+import CardContent from '@mui/material/CardContent';
+import CardMedia from '@mui/material/CardMedia';
+import Typography from '@mui/material/Typography';
+import { CardActionArea } from '@mui/material';
 
 // Custom Style React-Select
 const customStyles = {
@@ -72,15 +77,17 @@ function Shop(params) {
     const [minPrice, setMinPrice] = useState(0)
     const [maxPrice, setMaxPrice] = useState(999999999999999)
 
-    useEffect(()=>{
-        client.get("/items", {params: {
-            search: search,
-            harga_min: minPrice,
-            harga_max: maxPrice,
-        }}
-        ).then((res)=>{
+    useEffect(() => {
+        client.get("/items", {
+            params: {
+                search: search,
+                harga_min: minPrice,
+                harga_max: maxPrice,
+            }
+        }
+        ).then((res) => {
             setListItem(res.data)
-        }).catch((err)=>{
+        }).catch((err) => {
             console.log(err)
         })
     }, [search, minPrice, maxPrice])
@@ -109,18 +116,18 @@ function Shop(params) {
                             options={optionBox}
                             defaultInputValue={categoryFilter}
                             styles={customStyles}
-                            onChange={(e) => {dispatch(setCategory(e.value))}}
+                            onChange={(e) => { dispatch(setCategory(e.value)) }}
                         />
                     </div>
                     <div className='w-full'>
                         <span className="text-putih text-xl font-bold my-2">Harga (Rp)</span>
-                        <input type="number" placeholder="Harga Minimum" className="border-2 border-oranye bg-abu-gelap text-white text-lg rounded-lg my-2 w-full px-3 py-1" step={1000} min={0} max={999999999999999} value={minPrice} onChange={(e)=>{if(e.target.value != "") setMinPrice(e.target.value)}}/>
-                        <input type="number" placeholder="Harga Maximum" className="border-2 border-oranye bg-abu-gelap text-white text-lg rounded-lg mb-2 w-full px-3 py-1" step={1000} min={0} max={999999999999999} value={maxPrice} onChange={(e)=>{if(e.target.value != "") setMaxPrice(e.target.value)}}/>
+                        <input type="number" placeholder="Harga Minimum" className="border-2 border-oranye bg-abu-gelap text-white text-lg rounded-lg my-2 w-full px-3 py-1" step={1000} min={0} max={999999999999999} value={minPrice} onChange={(e) => { if (e.target.value != "") setMinPrice(e.target.value) }} />
+                        <input type="number" placeholder="Harga Maximum" className="border-2 border-oranye bg-abu-gelap text-white text-lg rounded-lg mb-2 w-full px-3 py-1" step={1000} min={0} max={999999999999999} value={maxPrice} onChange={(e) => { if (e.target.value != "") setMaxPrice(e.target.value) }} />
                     </div>
                     <div>
                         <span className="text-putih">Rating</span>
                         <div className="flex justify-between place-items-center">
-                            <input type="checkbox" className="w-5 h-5"/>
+                            <input type="checkbox" className="w-5 h-5" />
                             <Stack spacing={1} className="bg-white h-max rounded-xl p-1">
                                 <Rating name="half-rating" defaultValue={2.5} precision={0.5} />
                             </Stack>
@@ -134,27 +141,50 @@ function Shop(params) {
 
                     {listItem && listItem.map((item) => {
                         if (categoryFilter == "All" || item.category == categoryFilter)
-                        return (
-                            <Link to={`/barang/${item._id}`}>
-                            <div className='bg-abu-gelap w-48 h-60 rounded-xl flex flex-col border-2 mt-14'>
-                                <div className='h-2/3 flex flex-col-reverse rounded-xl bg-white bg-[url("/img/graphic_card.png")] bg-center'>
-                                    <img src={import.meta.env.VITE_BACKEND_GET_PICTURE_URL+item.picture} alt="" />
-                                    {/* <div className='grid grid-cols-3 gap-1'>
-                                        <div className='col-start-3 text-white'>5<StarIcon sx={{ color: yellow[500] }} /></div>
-                                    </div> */}
-                                </div>
-                                <div className='h-1/3'>
-                                    <div className='grid grid-rows-4'>
-                                        <div className='row-span-2 text-white font-bold text-xl'>{item.name}</div>
-                                        {item.discount && 
-                                            <div className='text-oranye font-bold text-lg'>Rp {item.discount.promo_price.toLocaleString('id-ID')}</div>
-                                        }
-                                        <div className={'text-oranye font-bold text-lg' + (item.discount?" line-through":"")}>Rp {item.price.toLocaleString('id-ID')}</div>
-                                    </div>
-                                </div>
-                            </div>
-                            </Link>
-                        )
+                            return (
+                                // <Link to={`/barang/${item._id}`}>
+                                // <div className='bg-abu-gelap w-48 h-60 rounded-xl flex flex-col border-2 mt-14'>
+                                //     <div className='h-2/3 flex flex-col-reverse rounded-xl bg-white bg-[url("/img/graphic_card.png")] bg-center'>
+                                //         <img src={import.meta.env.VITE_BACKEND_GET_PICTURE_URL+item.picture} alt="" />
+                                //         {/* <div className='grid grid-cols-3 gap-1'>
+                                //             <div className='col-start-3 text-white'>5<StarIcon sx={{ color: yellow[500] }} /></div>
+                                //         </div> */}
+                                //     </div>
+                                //     <div className='h-1/3'>
+                                //         <div className='grid grid-rows-4'>
+                                //             <div className='row-span-2 text-white font-bold text-xl'>{item.name}</div>
+                                //             {item.discount && 
+                                //                 <div className='text-oranye font-bold text-lg'>Rp {item.discount.promo_price.toLocaleString('id-ID')}</div>
+                                //             }
+                                //             <div className={'text-oranye font-bold text-lg' + (item.discount?" line-through":"")}>Rp {item.price.toLocaleString('id-ID')}</div>
+                                //         </div>
+                                //     </div>
+                                // </div>
+                                // </Link>
+                                <Card sx={{ maxWidth: 345 }}>
+                                    <CardActionArea>
+                                        <CardMedia
+                                            component="img"
+                                            height="140"
+                                            image={import.meta.env.VITE_BACKEND_GET_PICTURE_URL + item.picture}
+                                            alt=""
+                                        />
+                                        <CardContent>
+                                            <Typography gutterBottom variant="h5" component="div">
+                                                {item.name}
+                                            </Typography>
+                                            <Typography variant="body1" color="text.secondary">
+                                                {item.discount &&
+                                                    <div className='text-oranye font-bold text-lg'>Rp {item.discount.promo_price.toLocaleString('id-ID')}</div>
+                                                }
+                                            </Typography>
+                                            <Typography variant="body2" color="text.secondary" className={(item.discount?" line-through":"")}>
+                                                Rp {item.price.toLocaleString('id-ID')}
+                                            </Typography>
+                                        </CardContent>
+                                    </CardActionArea>
+                                </Card>
+                            )
                     })}
 
                 </div>
