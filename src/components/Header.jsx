55ca304baa9cb3947c16510b476b6c4a0928c08e
PaddyHermanito <paddy.hermanito.suhartanto93@gmail.com>
2023-12-06 18:31:30 +0700

    desain header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -75,22 +75,29 @@ function Header(){
     }
 
     return(
-        <nav className="grid grid-cols-7 bg-abu-super-gelap place-items-center h-[5rem]">
+        <nav className="grid grid-cols-8 bg-abu-super-gelap place-items-center h-[5rem]">
             <NavLink to="/">
-                <div className="bg-abu-super-gelap text-white rounded-lg text-3xl p-2 ms-10 flex justify-center"><ComputerIcon fontSize='large'/>&nbsp;Compute&nbsp;<span className="bg-oranye rounded-lg font-bold text-abu-super-gelap px-1">HUB</span></div>
+                <div className="bg-abu-super-gelap text-white rounded-lg text-2xl p-2 ms-10 flex justify-center"><ComputerIcon fontSize='large'/>&nbsp;Compute&nbsp;<span className="bg-oranye rounded-lg font-bold text-abu-super-gelap px-1">HUB</span></div>
             </NavLink>
             <NavLink to="/shop" className={(state)=>`text-white text-2xl ${state.isActive ? "font-bold text-3xl" : ""}`}>
                 Shop
             </NavLink>
-            <div className='flex items-center'>
+            <NavLink to={"/build"} className={(state)=>`text-white text-2xl ${state.isActive ? "font-bold text-3xl" : ""}`}>
+                Build
+            </NavLink>
+            {/* <div className='flex items-center'> */}
+                <form onSubmit={handleSubmit(searching)} className='flex items-center col-span-2 w-full'>
+                    <input type="text" className='w-full rounded-xl px-4 py-2' placeholder='Search Product..' defaultValue={searchFilter} {...register("search")}/>
+                    <div className="text-white text-2xl col-span-2 ms-2"><SearchIcon fontSize='large'/></div>
+                </form>
+            {/* </div> */}
+            {/* <div className='flex items-center'>
                 <form onSubmit={handleSubmit(searching)}>
                     <input type="text" className='w-[14rem] rounded-xl px-4 py-2' placeholder='Search Product..' defaultValue={searchFilter} {...register("search")}/>
                 </form>
                 <div className="text-white text-2xl col-span-2 ms-2"><SearchIcon fontSize='large'/></div>
-            </div>
-            <NavLink to={"/build"} className={(state)=>`text-white text-2xl ${state.isActive ? "font-bold text-3xl" : ""}`}>
-                Build
-            </NavLink>
+            </div> */}
+            
             <NavLink to="/cart" className={(state)=>`text-white text-2xl ${state.isActive ? "font-bold text-3xl" : ""}`}>
                 Cart
             </NavLink>
