1800fe9cc145f4456dd8ef38d504e7df212061f0
PaddyHermanito <105158168+PaddyHermanito@users.noreply.github.com>
2024-03-06 09:36:28 +0700

    Update warna dashboardTheme.js

--- dashboardTheme.js
+++ dashboardTheme.js
@@ -3,8 +3,8 @@ import { createTheme, ThemeProvider, styled } from '@mui/material/styles';
 const dashboardTheme = createTheme({
     palette: {
         primary: {
-            main: '#ffa31a',
-            oranye:"#ffa31a",
+            main: '#1a90ff',
+            oranye:"#1a90ff",
             abu_abu:"#808080",
             abu_gelap:"#292929",
             abu_super_gelap:"#1b1b1b",
